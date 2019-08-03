2d7d6e9706bafb6d87d464d6dda63cf5535c4264
Nicolas Gehlert <nicolas.gehlert@gmail.com>
2019-08-03 13:52:13 +0200

    fixed 404 page for build

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,10 +7,11 @@ import SEO from "../components/seo";
 class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props;
+    const siteLogo = data.logo;
     const siteTitle = data.site.siteMetadata.title;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} title={siteTitle} logo={siteLogo}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
@@ -23,6 +24,13 @@ export default NotFoundPage;
 
 export const pageQuery = graphql`
   query {
+    logo: file(absolutePath: { regex: "/logo.png/" }) {
+      childImageSharp {
+        fixed(width: 200, height: 200) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     site {
       siteMetadata {
         title
