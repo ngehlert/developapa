be9f60bdea0f8c6ae306aa79016046c60dacef67
Nicolas Gehlert <nicolas.gehlert@gmail.com>
2019-08-10 13:33:18 +0200

    finished support for comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { rhythm, scale } from "../utils/typography";
@@ -10,15 +9,22 @@ import Button from "@material-ui/core/Button"
 import { Theme } from "../pages"
 import { ThemeProvider } from '@material-ui/styles';
 import * as axios from "axios"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
+import FormGroup from "@material-ui/core/FormGroup"
 
 class BlogPostTemplate extends React.Component {
 
   state = {
     commentName: '',
     commentMessage: '',
+    gdpr: false,
   };
   render() {
-    console.log(this.props.data)
+    const comments = this.props.data.allYaml.edges;
     const post = this.props.data.markdownRemark;
     const siteLogo = this.props.data.logo;
     const siteTitle = this.props.data.site.siteMetadata.title;
@@ -57,9 +63,45 @@ class BlogPostTemplate extends React.Component {
           </p>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
           {divider}
-          <Bio />
+          <h4>Comments</h4>
 
-          <h3>Add a comment</h3>
+          {comments
+            .sort(({node: nodeA}, {node: nodeB}) => {
+              return nodeA.date - nodeB.date;
+            })
+            .map(({node}) => {
+              const options = {
+                year: 'numeric', month: 'long', day: 'numeric',
+                hour: 'numeric', minute: 'numeric', second: 'numeric',
+                hour12: false,
+              };
+
+              return (
+                <div key={node.id} style={{marginBottom: rhythm(1)}}>
+                  <Card>
+                    <CardContent>
+                      <Typography variant="h6" component="h2">
+                        {node.name}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        {new Intl.DateTimeFormat('en-US', options).format(node.date * 1000)}
+                      </Typography>
+                      <Typography variant="body1" component="p">
+                        {node.message}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+              )
+            })
+          }
+          {comments.length === 0 ? (
+            <Typography variant="body1" color="textSecondary" component="p">
+              There are no comments available for this blog post yet
+            </Typography>
+          ): (null)}
+
+          <h4>Add a comment</h4>
           <form
             autoComplete="off"
           >
@@ -91,7 +133,24 @@ class BlogPostTemplate extends React.Component {
               margin="normal"
               variant="outlined"
             />
-            <Button variant="contained" color="primary" onClick={() => {
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.gdpr}
+                    onChange={(event) => {
+                      this.setState({
+                        gdpr: event.target.checked,
+                      })
+                    }}
+                    value="checkedGdpr"
+                    color="primary"
+                  />
+                }
+                label="I agree that my name will be stored in connection with my comment and will be visible to others after a review. To change/delete the comment later please contact me via mail at info [at] ngehlert.de"
+              />
+            </FormGroup>
+            <Button disabled={!this.state.gdpr} style={{marginTop: rhythm(1)}} variant="contained" color="primary" onClick={() => {
               this.saveComment();
             }}>
               Submit
